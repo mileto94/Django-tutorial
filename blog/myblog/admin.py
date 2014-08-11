c7ac48efa6cd8b87127f358ed372702f2431cb91
milka <mileto94@abv.bg>
2014-08-11 20:49:26 +0300

    Make models able to print

--- blog/myblog/admin.py
+++ blog/myblog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from myblog.models import Article, Author
 # Register your models here.
+
+admin.site.register(Article)
+admin.site.register(Author)
--- blog/myblog/models.py
+++ blog/myblog/models.py
@@ -1,11 +1,22 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 
 class Author(models.Model):
     name = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return self.name
 
 
 class Article(models.Model):
     text = models.CharField(max_length=1000)
     pub_date = models.DateTimeField("date published")
     author = models.ForeignKey(Author)
+
+    def __str__(self):
+        return self.text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
