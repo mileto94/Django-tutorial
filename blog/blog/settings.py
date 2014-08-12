08048175336c16f51847644ddb49cc92624c2303
milka <mileto94@abv.bg>
2014-08-12 15:54:11 +0300

    Add bootsrap to index.htnl

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -13,7 +13,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
-TEMPLATE_DIRS = [os.path.join(BASE_DIR, "templates")]
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, "templates"),
+                 os.path.join(BASE_DIR, "templates/myblog")]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,12 +1,14 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
 from myblog.models import Article
+from django.template import RequestContext, loader
 
 
 def index(request):
     latest_articles = Article.objects.order_by("-pub_date")[:5]
-    output = ", ".join([a.text for a in latest_articles])
-    return HttpResponse(output)
+    template = loader.get_template("myblog/index.html")
+    context = RequestContext(request, {"latest_articles": latest_articles})
+    return HttpResponse(template.render(context))
 
 
 def show_article(request, article_id):
