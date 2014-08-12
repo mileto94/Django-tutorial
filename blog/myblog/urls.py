9bc5b7aeb46ae7fd832634eeb280ac6c660d28b0
milka <mileto94@abv.bg>
2014-08-12 15:23:45 +0300

    Add new page

---
+++ blog/myblog/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+
+from myblog import views
+
+urlpatterns = patterns("",
+                       url(r"^$", views.index, name="index"),
+                       url(r"^(?P<article_id>\d+)/$", views.show_article,
+                           name="show_article"),
+                       )
--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
+from myblog.models import Article
 
-# Create your views here.
+
+def index(request):
+    latest_articles = Article.objects.order_by("-pub_date")[:5]
+    output = ", ".join([a.text for a in latest_articles])
+    return HttpResponse(output)
+
+
+def show_article(request, article_id):
+    return HttpResponse("You're looking for %s" % article_id)
