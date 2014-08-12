976319b4783bb6c4e7c351f8c0e1dc22117a806e
milka <mileto94@abv.bg>
2014-08-12 16:20:37 +0300

    Refactor views.py using render

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,14 +1,12 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 from myblog.models import Article
-from django.template import RequestContext, loader
 
 
 def index(request):
     latest_articles = Article.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("myblog/index.html")
-    context = RequestContext(request, {"latest_articles": latest_articles})
-    return HttpResponse(template.render(context))
+    context = {"latest_articles": latest_articles}
+    return render(request, "myblog/index.html", context)
 
 
 def show_article(request, article_id):
