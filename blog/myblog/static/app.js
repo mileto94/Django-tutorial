f818d80e2a5ac91208456434f57b3b83e0a080e2
milka <mileto94@abv.bg>
2014-08-18 10:02:17 +0300

    Add js file and attach it to base.html

---
+++ blog/myblog/static/app.js
@@ -0,0 +1,11 @@
+/* globals $, alert */
+
+$('#loading-example-btn').click(function() {
+    'use strict';
+    var btn = $(this);
+    btn.button('loading');
+    // $.ajax().always(function() {
+    //     tn.button('reset');
+    // });
+    alert('HHEHHEHEHEHY');
+});
