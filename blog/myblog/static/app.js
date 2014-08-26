0066b2d147afe4765f8ad6fa79461f281a9fcb71
milka <mileto94@abv.bg>
2014-08-26 12:33:28 +0300

    Back-buttons work again after extending from base.html

--- blog/myblog/static/app.js
+++ blog/myblog/static/app.js
@@ -1,20 +1,11 @@
 /* globals $, console, document, window */
 
-// $("#loading-example-btn").click(function() {
-//     var btn = $(this);
-//     btn.button("loading");
-//     // $.ajax().always(function() {
-//     //     tn.button("reset");
-//     // });
-//     alert("HHEHHEHEHEHY");
-// });
-
 $(document).ready(function() {
     "use strict";
 
-    function goBack() {
+    $("#back").on("click", function(){
         window.history.back();
-    }
+    });
 
     $(".like").on("click", function() {
         var rated = $("#liked").data().rating;
