b0dfddecba49ab699406a97b742242d9f4e1dd0f
milka <mileto94@abv.bg>
2014-09-08 17:17:41 +0300

    Heroku stuff

--- settings.py
+++ settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 
 
-# BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, "templates"),
